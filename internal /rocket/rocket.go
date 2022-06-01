16bedf4afe16790664320dfcabeea84290f6d1bf
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-06-01 12:48:30 +0200

    Update

--- internal /rocket/rocket.go
+++ internal /rocket/rocket.go
@@ -1,8 +1,14 @@
 package rocket
 
+// Rocket - should contain the definition of rocket
 type Rocket struct {
 	ID      string
 	Name    string
 	Type    string
 	Flights int
+}
+
+// Service responsible for updating the rocket
+type Service struct {
+	// TODO: empty for now
 }
