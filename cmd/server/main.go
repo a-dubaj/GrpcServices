24957fb6acab72caf7dfa824abd990eaf2ec08be
Andrzej Dubaj <dubaj.andrzej@gmail.com>
2022-06-01 12:44:11 +0200

    first commit

---
+++ cmd/server/main.go
@@ -0,0 +1,14 @@
+package server
+
+import "log"
+
+func Run() error {
+	// responsible for initializing and starting our gRPC server
+	return nil
+}
+
+func main() {
+	if err := Run(); err != nil {
+		log.Fatal(err)
+	}
+}
